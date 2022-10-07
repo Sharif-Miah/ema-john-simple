fad7e16da6c9fb5b0520945d0357750061467ca1
farhan sharif <farhansharif1410@gmail.com>
2022-10-07 12:35:34 +0530

    dynamic api call in react router

--- src/component/Order/Order.jsx
+++ src/component/Order/Order.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const Order = () => {
+    const order = useLoaderData();
+    console.log(order);
     return (
         <div>
-            <h1>This is Order component section</h1>
+            <h1>This is Order component section {order.length}</h1>
         </div>
     );
 };
