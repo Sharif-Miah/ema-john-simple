1c486cf7160b2a765bda24794ec1ed3191c6b0d3
farhan sharif <farhansharif1410@gmail.com>
2022-10-08 11:28:16 +0530

    review item design added

--- src/component/ReviewItem/ReviewItem.jsx
+++ src/component/ReviewItem/ReviewItem.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './ReviewItem.css'
+import { TrashIcon } from '@heroicons/react/24/solid'
 
 const ReviewItem = ({ product }) => {
     console.log(product);
@@ -7,6 +8,23 @@ const ReviewItem = ({ product }) => {
     return (
         <div className='review-container'>
             <img src={img} alt="" />
+            <div className='review-content'>
+                <div className='single-item'>
+
+                    <p>{name}</p>
+                    <p>Price<small>{price}</small></p>
+                    <p><small>Shipping Charge: {shipping}</small></p>
+                    <p>Quantity:<small> {quantity}</small></p>
+                </div>
+                <div className='delate-btn'>
+
+                    <button>
+                        <TrashIcon />
+                    </button>
+
+                </div>
+            </div>
+
         </div>
     );
 };
